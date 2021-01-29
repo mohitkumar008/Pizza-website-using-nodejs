d4a2b7f3d0be7fc03a0c845f1b41edc4c26b13d1
mohitkumar008 <mohitkumar0182000@gmail.com>
2021-01-30 00:58:15 +0530

    login, logout, register functionality

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose');
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
 
 
 //Database connection
@@ -37,17 +38,27 @@ app.use(session({
     cookie: {maxAge: 1000 * 60 * 60 * 24} //24 hours
 }))
 
+//Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Flash config
 app.use(flash());
 
 
 //Assets
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 
 
 //Global Middleware
 app.use((req, res, next) => {
     res.locals.session = req.session
+    res.locals.user = req.user
+    res.locals.login = req.login
     next()
 })
 
