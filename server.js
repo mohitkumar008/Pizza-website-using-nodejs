d6339bc5a684ef28c1afb6bd9cc117eb45d30b3c
mohitkumar008 <mohitkumar0182000@gmail.com>
2021-01-29 14:11:13 +0530

    add to cart functionallity

--- server.js
+++ server.js
@@ -1,13 +1,55 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
-
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')(session)
+
+
+//Database connection
+const url = 'mongodb://localhost/pizza';
+mongoose.connect(url, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database Connected...')
+}).catch(err => {
+    console.log('Connection failed')
+});
+
+
+// Session store
+let mongoStore = new MongoDbStore({
+    mongooseConnection: connection,
+    collection: 'sessions'
+})
+
+// Session Configration
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24} //24 hours
+}))
+
+app.use(flash());
+
 
 //Assets
 app.use(express.static('public'));
+app.use(express.json())
+
+
+//Global Middleware
+app.use((req, res, next) => {
+    res.locals.session = req.session
+    next()
+})
 
 
 //Set the template engine
